992ff42d13b006eb610bc1cc764c3935383d5f65
gitwilson <wyhc@hotmail.com>
2015-06-26 00:09:09 +0800

    Change function async.series() to async.each() to simplify the code for rule initialization. After change, the step does not follow the order of rule index. But the final completion message will be displayed lastly.

--- app.js
+++ app.js
@@ -16,167 +16,40 @@ var rules_obj = require('./routes/rules');
 rules = rules_obj.router; // Get the rules middleware
 
 /* Settle the rules before processing */
-async.series([
-	//Check the first rule, rule1, if it is already saved in DB
-	function(callback) {
-		var collection = db.collection('rulelist');
-		collection.find({'name': 'rule1'}).toArray(	function(err, store){
-			var R1 = new RuleEngine();
-			if((err == null)&& (store.length > 0)) {
-				var rule_store = store[0]["store"];
-				R1.fromJSON(rule_store);
-				var rule1 = R1.findRules({"name": "rule1"});
-				if (rule1.length > 0) {
-					// This rule is already existing, no need to save again
-					callback();
-					return ;
+var rule_idx = [0, 1, 2, 3, 4, 5];
+var rule_names = ["rule1", "rule2", "rule3", "rule4", "rule5", "rule6"];
+var rule_items = [rules_obj.rule1, rules_obj.rule2, rules_obj.rule3,
+                  rules_obj.rule4, rules_obj.rule5, rules_obj.rule6];
+async.each(rule_idx, // 1st param is the array of index
+		// 2nd param is the function that checks and save each rule into DB
+		function(idx, callback){
+			var collection = db.collection('rulelist');
+			collection.find({'name': rule_names[idx]}).toArray(	function(err, store){
+				var R1 = new RuleEngine();
+				if((err == null)&& (store.length > 0)) {
+					var rule_store = store[0]["store"];
+					R1.fromJSON(rule_store);
+					var rule = R1.findRules({"name": rule_names[idx]});
+					if (rule.length > 0) {
+						// This rule is already existing, no need to save again
+						callback();
+						return ;
+					}
 				}
-			}
-			// Somehow, this rule does not exist in DB, need to save it now
-			var R2 = new RuleEngine(rules_obj.rule1);
-			var rule_store = R2.toJSON();
-			store = {"name": "rule1", "store": rule_store};
-			db.collection('rulelist').insert(store, function(err, result){  
-				// we are going to insert 'rule1' to 'rulelist'
-				if (err ) return callback(err);
-				callback();
-			});
-		});
-	},
-	//Check the second rule, rule2, if it is already saved in DB
-	function(callback) {
-		var collection = db.collection('rulelist');
-		collection.find({'name': 'rule2'}).toArray(	function(err, store){
-			var R1 = new RuleEngine();
-			if((err == null)&& (store.length > 0)) {
-				var rule_store = store[0]["store"];
-				R1.fromJSON(rule_store);
-				var rule1 = R1.findRules({"name": "rule2"});
-				if (rule1.length > 0) {
-					// This rule is already existing, no need to save again
+				// Somehow, this rule does not exist in DB, need to save it now
+				var R2 = new RuleEngine(rule_items[idx]);
+				var rule_store = R2.toJSON();
+				store = {"name": rule_names[idx], "store": rule_store};
+				db.collection('rulelist').insert(store, function(err, result){  
+					// we are going to insert this rule to 'rulelist'
+					if (err ) return callback(err);
 					callback();
-					return ;
-				}
-			}
-			// Somehow, this rule does not exist in DB, need to save it now
-			var R2 = new RuleEngine(rules_obj.rule2);
-			var rule_store = R2.toJSON();
-			store = {"name": "rule2", "store": rule_store};
-			db.collection('rulelist').insert(store, function(err, result){  
-				// we are going to insert 'rule2' to 'rulelist'
-				if (err ) return callback(err);
-				callback();
+				});
 			});
-		});
-	},
-	//Check the third rule, rule3, if it is already saved in DB
-	function(callback) {
-		var collection = db.collection('rulelist');
-		collection.find({'name': 'rule3'}).toArray(	function(err, store){
-			var R1 = new RuleEngine();
-			if((err == null)&& (store.length > 0)) {
-				var rule_store = store[0]["store"];
-				R1.fromJSON(rule_store);
-				var rule3 = R1.findRules({"name": "rule3"});
-				if (rule3.length > 0) {
-					// This rule is already existing, no need to save again
-					callback();
-					return ;
-				}
-			}
-			// Somehow, this rule does not exist in DB, need to save it now
-			var R2 = new RuleEngine(rules_obj.rule3);
-			var rule_store = R2.toJSON();
-			store = {"name": "rule3", "store": rule_store};
-			db.collection('rulelist').insert(store, function(err, result){  
-				// we are going to insert 'rule3' to 'rulelist'
-				if (err ) return callback(err);
-				callback();
-			});
-		});
-	},
-	//Check the fourth rule, rule4, if it is already saved in DB
-	function(callback) {
-		var collection = db.collection('rulelist');
-		collection.find({'name': 'rule4'}).toArray(	function(err, store){
-			var R1 = new RuleEngine();
-			if((err == null)&& (store.length > 0)) {
-				var rule_store = store[0]["store"];
-				R1.fromJSON(rule_store);
-				var rule4 = R1.findRules({"name": "rule4"});
-				if (rule4.length > 0) {
-					// This rule is already existing, no need to save again
-					callback();
-					return ;
-				}
-			}
-			// Somehow, this rule does not exist in DB, need to save it now
-			var R2 = new RuleEngine(rules_obj.rule4);
-			var rule_store = R2.toJSON();
-			store = {"name": "rule4", "store": rule_store};
-			db.collection('rulelist').insert(store, function(err, result){  
-				// we are going to insert 'rule4' to 'rulelist'
-				if (err ) return callback(err);
-				callback();
-			});
-		});
-	},
-	//Check the fifth rule, rule5, if it is already saved in DB
-	function(callback) {
-		var collection = db.collection('rulelist');
-		collection.find({'name': 'rule5'}).toArray(	function(err, store){
-			var R1 = new RuleEngine();
-			if((err == null)&& (store.length > 0)) {
-				var rule_store = store[0]["store"];
-				R1.fromJSON(rule_store);
-				var rule5 = R1.findRules({"name": "rule5"});
-				if (rule5.length > 0) {
-					// This rule is already existing, no need to save again
-					callback();
-					return ;
-				}
-			}
-			// Somehow, this rule does not exist in DB, need to save it now
-			var R2 = new RuleEngine(rules_obj.rule5);
-			var rule_store = R2.toJSON();
-			store = {"name": "rule5", "store": rule_store};
-			db.collection('rulelist').insert(store, function(err, result){  
-				// we are going to insert 'rule5' to 'rulelist'
-				if (err ) return callback(err);
-				callback();
-			});
-		});
-	},
-	//Check the sixth rule, rule6, if it is already saved in DB
-	function(callback) {
-		var collection = db.collection('rulelist');
-		collection.find({'name': 'rule6'}).toArray(	function(err, store){
-			var R1 = new RuleEngine();
-			if((err == null)&& (store.length > 0)) {
-				var rule_store = store[0]["store"];
-				R1.fromJSON(rule_store);
-				var rule6 = R1.findRules({"name": "rule6"});
-				if (rule6.length > 0) {
-					// This rule is already existing, no need to save again
-					callback();
-					return ;
-				}
-			}
-			// Somehow, this rule does not exist in DB, need to save it now
-			var R2 = new RuleEngine(rules_obj.rule6);
-			var rule_store = R2.toJSON();
-			store = {"name": "rule6", "store": rule_store};
-			db.collection('rulelist').insert(store, function(err, result){  
-				// we are going to insert 'rule6' to 'rulelist'
-				if (err ) return callback(err);
-				callback();
-			});
-		});
-	}
-	//Load posts (won't be called before task 1's "task callback" has been called)
-    ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
-		if (err) return (err);
-	}
+		}, 
+		function(err) { //This function gets called after the two tasks have called their "task callbacks"
+			if (err) return (err);
+		}
 );
 
 var app = express();
